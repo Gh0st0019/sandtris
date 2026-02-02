cce22470a69e262c04ce2d27d7817bf0fa6a8e3e
Ch3rry <brociaociao02@gmail.com>
2026-02-02 19:16:29 +0100

    Center score-only game banner

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260202-18";
+const CACHE_NAME = "sandtris-v20260202-19";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260202-18",
-  "./app.js?v=20260202-18",
-  "./manifest.webmanifest?v=20260202-18",
+  "./style.css?v=20260202-19",
+  "./app.js?v=20260202-19",
+  "./manifest.webmanifest?v=20260202-19",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/maskable-icon.png",
