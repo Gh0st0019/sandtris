96af1bb5a2176443bd24a4903fdf8f3194748541
Ch3rry <brociaociao02@gmail.com>
2026-02-02 19:31:51 +0100

    Use exit icon for home button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260202-19";
+const CACHE_NAME = "sandtris-v20260202-20";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260202-19",
-  "./app.js?v=20260202-19",
-  "./manifest.webmanifest?v=20260202-19",
+  "./style.css?v=20260202-20",
+  "./app.js?v=20260202-20",
+  "./manifest.webmanifest?v=20260202-20",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/maskable-icon.png",
@@ -26,6 +26,7 @@ const ASSETS = [
   "./assets/favicon.ico",
   "./assets/favicon-32.png",
   "./assets/crown.png",
+  "./assets/exit.png",
 ];
 
 let messaging = null;
