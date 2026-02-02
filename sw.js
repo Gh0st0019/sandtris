4589a311b02408fda3047ccdd6b491d524a65db5
Ch3rry <brociaociao02@gmail.com>
2026-02-02 19:51:27 +0100

    Reduce exit button to 45px

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260202-24";
+const CACHE_NAME = "sandtris-v20260202-25";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260202-24",
-  "./app.js?v=20260202-24",
-  "./manifest.webmanifest?v=20260202-24",
+  "./style.css?v=20260202-25",
+  "./app.js?v=20260202-25",
+  "./manifest.webmanifest?v=20260202-25",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/maskable-icon.png",
