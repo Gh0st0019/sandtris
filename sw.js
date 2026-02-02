b3468430f875566decade32522ab77b9a41a7da0
Ch3rry <brociaociao02@gmail.com>
2026-02-02 19:34:40 +0100

    Double exit icon size

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260202-20";
+const CACHE_NAME = "sandtris-v20260202-21";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260202-20",
-  "./app.js?v=20260202-20",
-  "./manifest.webmanifest?v=20260202-20",
+  "./style.css?v=20260202-21",
+  "./app.js?v=20260202-21",
+  "./manifest.webmanifest?v=20260202-21",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/maskable-icon.png",
