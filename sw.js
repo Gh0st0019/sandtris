96a90a56602dfbf26d9ac79695e14a8f1ddced52
Ch3rry <brociaociao02@gmail.com>
2026-02-02 19:59:35 +0100

    Set exit icon to 40px

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260202-26";
+const CACHE_NAME = "sandtris-v20260202-27";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260202-26",
-  "./app.js?v=20260202-26",
-  "./manifest.webmanifest?v=20260202-26",
+  "./style.css?v=20260202-27",
+  "./app.js?v=20260202-27",
+  "./manifest.webmanifest?v=20260202-27",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/maskable-icon.png",
