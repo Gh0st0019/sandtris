991f94af9ee8b07fc098d0829d016efd9328dec8
Ch3rry <brociaociao02@gmail.com>
2026-02-04 21:52:44 +0100

    Enlarge report badge

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260204-24";
+const CACHE_NAME = "sandtris-v20260204-25";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260204-24",
-  "./app.js?v=20260204-24",
-  "./manifest.webmanifest?v=20260204-24",
+  "./style.css?v=20260204-25",
+  "./app.js?v=20260204-25",
+  "./manifest.webmanifest?v=20260204-25",
   "./assets/gmail.jpg",
   "./assets/report.png",
   "./assets/icon-192.png",
