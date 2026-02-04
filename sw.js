03e2a98421c52d23a2a48631987e42777005c41a
Ch3rry <brociaociao02@gmail.com>
2026-02-04 21:16:52 +0100

    Add AdSense script

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260204-19";
+const CACHE_NAME = "sandtris-v20260204-20";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260204-19",
-  "./app.js?v=20260204-19",
-  "./manifest.webmanifest?v=20260204-19",
+  "./style.css?v=20260204-20",
+  "./app.js?v=20260204-20",
+  "./manifest.webmanifest?v=20260204-20",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/maskable-icon.png",
