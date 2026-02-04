eb9ab5bc6a4f0ae0102a69b2ce37c3e1ad401de0
Ch3rry <brociaociao02@gmail.com>
2026-02-04 21:55:01 +0100

    Move support badges left

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260204-25";
+const CACHE_NAME = "sandtris-v20260204-26";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260204-25",
-  "./app.js?v=20260204-25",
-  "./manifest.webmanifest?v=20260204-25",
+  "./style.css?v=20260204-26",
+  "./app.js?v=20260204-26",
+  "./manifest.webmanifest?v=20260204-26",
   "./assets/gmail.jpg",
   "./assets/report.png",
   "./assets/icon-192.png",
