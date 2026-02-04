47b421431c87670a0efb144cec15e2acc284b1b7
Ch3rry <brociaociao02@gmail.com>
2026-02-04 21:57:38 +0100

    Offset Gmail icon to the right

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260204-26";
+const CACHE_NAME = "sandtris-v20260204-27";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,9 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260204-26",
-  "./app.js?v=20260204-26",
-  "./manifest.webmanifest?v=20260204-26",
+  "./style.css?v=20260204-27",
+  "./app.js?v=20260204-27",
+  "./manifest.webmanifest?v=20260204-27",
   "./assets/gmail.jpg",
   "./assets/report.png",
   "./assets/icon-192.png",
