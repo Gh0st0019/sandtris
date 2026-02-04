8b5cf9334a8acdaf588c38b6ae227bb0d1587d7a
Ch3rry <brociaociao02@gmail.com>
2026-02-04 21:30:02 +0100

    Add Gmail support link

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sandtris-v20260204-20";
+const CACHE_NAME = "sandtris-v20260204-21";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCs669r3JZNH7vhnMtvHo_5TfQHIwYyHdM",
   authDomain: "sandtris-81990.firebaseapp.com",
@@ -15,9 +15,10 @@ const DEFAULT_NOTIFICATION = {
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260204-20",
-  "./app.js?v=20260204-20",
-  "./manifest.webmanifest?v=20260204-20",
+  "./style.css?v=20260204-21",
+  "./app.js?v=20260204-21",
+  "./manifest.webmanifest?v=20260204-21",
+  "./assets/gmail.jpg",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/maskable-icon.png",
